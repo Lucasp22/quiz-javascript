f8551e569aa8ed1b0de9175e62f2b444bdcb6275
Lucas Padua <lucas.p2222@gmail.ciom>
2018-09-08 14:38:15 +1000

    Controllers

---
+++ script.js
@@ -0,0 +1,19 @@
+  ///////////// Quiz controller/////////////////////
+  let quizController = (function() {
+
+
+
+  })();
+  ///////////// UI controller/////////////////////
+  let UIController = (function() {
+
+
+  })();
+
+  ///////////// Controller/////////////////////
+
+  let controller = (function(quizCtrl, UICtrl) {
+
+  
+
+  })(quizController, UIController);
