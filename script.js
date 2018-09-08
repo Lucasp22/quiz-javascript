d279bba823458857ff1c7fe3476d6f74ae366e54
Lucas Padua <lucas.p2222@gmail.ciom>
2018-09-08 14:51:09 +1000

    Testing LocalStorage

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
   ///////////// Quiz controller/////////////////////
   let quizController = (function() {
 
+    localStorage.setItem('data', JSON.stringify([1, 2, 4, 5]));
+    localStorage.setItem('data', JSON.stringify({name: 'John'}));
 
+    localStorage.removeItem('data');
+
+    console.log(JSON.parse(localStorage.getItem('data')));
 
   })();
   ///////////// UI controller/////////////////////
@@ -14,6 +19,6 @@
 
   let controller = (function(quizCtrl, UICtrl) {
 
-  
+
 
   })(quizController, UIController);
